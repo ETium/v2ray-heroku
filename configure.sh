d7c275a76ea66d939fba43532eb7f3e602d4f3c8
GHinStyle <38588395+GHinStyle@users.noreply.github.com>
2020-03-25 23:39:56 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -11,13 +11,6 @@ cat <<-EOF > /etc/v2ray/config.json
 {
   "inbounds": [
   {
-    "sniffing": {
-     "enabled": true,
-     "destOverride": [
-      "http",
-      "tls"
-     ]
-    },
     "port": ${PORT},
     "protocol": "vmess",
     "settings": {
@@ -26,49 +19,22 @@ cat <<-EOF > /etc/v2ray/config.json
           "id": "${UUID}",
           "alterId": 10
         }
-      ],
-	"disableInsecureEncryption": true
+      ]
     },
     "streamSettings": {
-     "network":"ws",
-     "wsSettings": {
+      "network": "ws",
+      "wsSettings": {
 	"path": "${PATH}"
 	}
     }
-   }
+  }
   ],
   "outbounds": [
   {
     "protocol": "freedom",
     "settings": {}
-  },
-  {
-    "protocol": "blackhole",
-    "settings": {},
-    "tag": "blocked"
   }
- ],
- "routing": {
-  "rules": [
-   {
-    "type": "field",
-    "outboundTag": "blocked",
-    "protocol": [
-     "bittorrent"
-    ]
-   }
   ]
- },
- "dns": {
-  "servers": [
-   "https+local://1.1.1.1/dns-query",
-   "1.1.1.1",
-   "1.0.0.1",
-   "8.8.8.8",
-   "8.8.4.4",
-   "localhost"
-  ]
-  }
 }
 EOF
 /usr/bin/v2ray/v2ray -config=/etc/v2ray/config.json
