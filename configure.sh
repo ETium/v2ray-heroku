7ee39a945038f1cd2454af72ef7562bd6009de35
GHinStyle <38588395+GHinStyle@users.noreply.github.com>
2020-04-06 18:33:12 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -3,6 +3,8 @@
 mkdir /tmp/v2ray
 curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/v2ray/v2ray-core/releases/latest/download/v2ray-linux-64.zip
 unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
+mkdir /usr/local/bin /etc/v2ray
+touch /etc/v2ray/config.json
 install -m 755 /tmp/v2ray/v2ray /usr/local/bin/v2ray
 install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
 # Remove temporary directory
